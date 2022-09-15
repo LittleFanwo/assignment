699f66f7af7c5d19cae11deaaafded3fdfd51a55
Little Falcon <113622554+LittleFanwo@users.noreply.github.com>
2022-09-16 02:57:54 +0630

    Update hw.js

--- hw.js
+++ hw.js
@@ -1,8 +1,20 @@
 const http = require('http');
+const fs = require('fs'); // Another library
+const port = 3000;
 
 function showData(req , res){
-    res.write("<h1>Hello World</h1>");
-    res.end();
+    res.writeHead(200 , { 'Content-Type' : 'text/html' });
+    fs.readFile('./main.html' , 'utf-8' ,function(err , data){
+        //Check the error
+        if (err){
+            res.writeHead(404); //Not Found
+            res.write('Error : File not Found :(');
+        }
+        else{
+            res.write(data);
+        }
+        res.end();
+    })
 }
 
-http.createServer(showData).listen(5000);
+http.createServer(showData).listen(port);
